bc4da9a5e1a63f248ccb7d9036cf7cacb4aa71ae
vishwajitvm <vishwajitmall50@gmail.com>
2023-06-04 01:53:24 +0530

    Complete frontend basic part , add styling in taskyfy

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import InputField from './components/InputField';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <span className='heading'>Taskify</span>
+      <InputField />
     </div>
   );
 }
---
+++ src/components/InputField.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles.css' ;
+
+const InputField = () => {
+  return (
+    <form className='input'>
+      <input type="text" placeholder='Enter a task' className='input__box' />
+      <button type='submit' className='input_submit'>Go</button>
+    </form>
+  )
+}
+
+export default InputField
