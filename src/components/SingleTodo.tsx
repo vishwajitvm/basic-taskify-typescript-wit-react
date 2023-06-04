3f7b1d9106f0f0a880f81c2038eba7dd1c77868b
vishwajitvm <vishwajitmall50@gmail.com>
2023-06-04 17:18:46 +0530

    MArk as done Completed

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -11,13 +11,25 @@ type Props = {
 }
 
 const SingleTodo = ({ todo , todos , setTodos }: Props) => {
+    //HANDEL MARK AS DONE 
+    const handeleDone = (id: number) => {
+        setTodos(todos.map(
+            (todo) => todo.id === id?{ ...todo,isDone: !todo.isDone }:todo)) 
+    }
   return (
     <form className='todos__single' >
-      <span className='todos__single--text'> { todo.todo } </span>
+        {
+            todo.isDone? (
+                <s className='todos__single--text'> { todo.todo } </s>
+            ):(
+                <span className='todos__single--text'> { todo.todo } </span>
+            )
+        }
+      {/* <span className='todos__single--text'> { todo.todo } </span> */}
       <div>
       <span className='icon'> <AiFillEdit /> </span>
       <span className='icon'> <AiFillDelete /> </span>
-      <span className='icon'> <MdDone /> </span>
+      <span className='icon' onClick={() => handeleDone(todo.id)} > <MdDone /> </span>
       </div>
     </form>
   )
