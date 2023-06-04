1d7be0696492d613476bcb3587e4a0cd00f0caab
vishwajitvm <vishwajitmall50@gmail.com>
2023-06-04 19:49:47 +0530

    Make remove scontainer

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -10,12 +10,28 @@ interface props {
 
 const TodoList: React.FC<props> = ({todos , setTodos}: props) => {
   return (
-    <div className='todos'>
-      {
-        todos.map(todo=> (
-           <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} /> //todos={todos} send to delete or edit or mark complete 
-        ))
-      }
+    <div className="container">
+      <div className="todos">
+        <span className="todos__heading">
+          Active Tasks
+        </span>
+        {
+          todos.map(todo => (
+            <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} /> //todos={todos} send to delete or edit or mark complete 
+          ))
+        }
+      </div>
+
+      <div className="todos remove">
+      <span className="todos__heading">
+          Completed Tasks
+        </span>
+        {
+          todos.map(todo => (
+            <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos} /> //todos={todos} send to delete or edit or mark complete 
+          ))
+        }
+      </div>
     </div>
   )
 }
